c2c99d7412b9fa55b7d02a4995272b2b264e19e8
Prince D Jele <Umlamulankunzi@gmail.com>
2023-10-30 08:11:29 +0200

    implemented task 5 solution

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Creates a Flask app"""
 
-from flask import Flask
+from os import getenv
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -15,3 +16,18 @@ app.url_map.strict_slashes = False
 def teardown_app(exception):
     """Handles app tear down"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 Error - Resource not found"""
+    response = {"error": "Not found"}
+    return (jsonify(response), 404)
+
+
+if __name__ == "__main__":
+    # get host & port from environment variables.
+    HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    PORT = int(getenv("HBNB_API_PORT", "5000"))
+
+    # Running app in multi thread to boost performance
+    app.run(host=HOST, port=PORT, threaded=True)
