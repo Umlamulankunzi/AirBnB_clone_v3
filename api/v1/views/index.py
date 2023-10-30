b88b8287d78f40ffac34fca1407af5fd0575d51c
Prince D Jele <Umlamulankunzi@gmail.com>
2023-10-30 07:53:42 +0200

    implemented task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Create a route '/status' on the object app_views
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"])
@@ -13,3 +14,16 @@ def api_status():
     """Returns status of RESTful API"""
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route("/stats", methods=["GET"])
+def get_stats():
+    """Get statistics of each object type"""
+    statistics = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("Amenity"),
+        "places": storage.count("Amenity"),
+        "reviews": storage.count("Amenity"),
+        "states": storage.count("Amenity"),
+        "users": storage.count("Amenity"),
+    }
+    return jsonify(statistics)
